3b732d4d2f56f28f89ac9c95c8c5e120013edb75
Alex Manning <alex.manning@serato.com>
2025-02-09 15:00:54 +1300

    Question 33 improvement

--- Medium/33_SearchInRotatedSortedArray.java
+++ Medium/33_SearchInRotatedSortedArray.java
@@ -1,47 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int back = nums[nums.length - 1];
-        int kValue = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = (right - left) / 2;
 
-        for( int i = nums.length - 2; i >= 0; i--){
-            if (nums[i] > back){
-                kValue = i + 1;
-                break;
+        while (left <= right){
+            if (target == nums[middle]) {
+                return middle; // Success!
             }
-            back = nums[i];
-        }
-
-        int value = -1;
-        int p1 = 0;
-        int p2 = nums.length;
-        int midPointIndex = ((p2 + p1) / 2);
-        int adjustedIndex = (midPointIndex + kValue) % nums.length;
-        value = nums[adjustedIndex];
-
-        while ( value != target ){
-            if ( p1+1 == p2 ){
-                return -1;
+            // In left sorted list
+            if (nums[middle] >= nums[left]) {
+                if (target > nums[middle] || target < nums[left]) {
+                    left = middle + 1;
+                    middle = (right + left) / 2;
+                }
+                else {
+                    right = middle - 1 ;
+                    middle = (right + left) / 2;
+                }
             }
 
-            if( value > target){
-                p1 = p1;
-                p2 = midPointIndex;
-                midPointIndex = ((p2 + p1) / 2);
-                adjustedIndex = (midPointIndex + kValue) % nums.length;
-                value = nums[adjustedIndex];
-                continue;
-            }
-
-            else if( value < target ){
-                p1 = midPointIndex;
-                p2 = p2;
-                midPointIndex = ((p2 + p1) / 2);
-                adjustedIndex = (midPointIndex + kValue) % nums.length;
-                value = nums[adjustedIndex];
-                continue;
+            // In right sorted list 
+            else {
+                if ( target < nums[middle] || target >= nums[left]) {
+                    right = middle - 1 ;
+                    middle = (right + left) / 2;
+                }
+                else {
+                    left = middle + 1;
+                    middle = (right + left) / 2;
+                }
             }
         }
 
-        return adjustedIndex;
+        return -1;
     }
 }
